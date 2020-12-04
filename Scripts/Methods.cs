e7a01809c2457d9c18d55eba8d88d53912e93f50
linearaccelerator <64271566+linearaccelerator@users.noreply.github.com>
2020-12-04 17:50:14 +0100

    Performance optimization for notation methods
    Edited: [Methods.cs](Scripts/Methods.cs) +8|-0

--- Scripts/Methods.cs
+++ Scripts/Methods.cs
@@ -9,23 +9,31 @@ public class Methods : MonoBehaviour
 {
     public static string NotationMethod(double x, string y)
     {
-        if (x >= 1000)
+        /* if (x >= 1000)
         {
             var exponent = Math.Floor(Math.Log10(Math.Abs(x)));
             var mantissa = x / Math.Pow(10, exponent);
             return $"{mantissa:F2}e{exponent:F0}";
         }
-        return x.ToString(y);
+        return x.ToString(y);*/
+        if (x < 1000) return x.ToString(y);
+        var exp = Math.Floor(Math.Log10(Math.Abs(x)));
+        var mant = x / Math.Pow(10, exp);
+        return $"{mant:F2}e{exp:F0}";
     }
 
     public static string NotationMethodBD(BigDouble x, string y)
     {
-        if (x >= 1000)
+        /* if (x >= 1000)
         {
             var exponent = Floor(Log10(Abs(x)));
             var mantissa = x / Pow(10, exponent);
             return $"{mantissa:F2}e{exponent:F0}";
         }
-        return x.ToString(y);
+        return x.ToString(y);*/
+        if (x < 1000) return x.ToString(y);
+        var exp = Floor(Log10(Abs(x)));
+        var mant = x / Pow(10, exp);
+        return $"{mant:F2}e{exp:F0}";
     }
 }
